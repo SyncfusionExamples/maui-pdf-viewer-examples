8d255ecc9c92dab8cc790b490b4c40c0822326c7
Deepak G <deepakgunasekaran@syncfusion.com>
2023-04-12 12:03:06 +0530

    Sample added for choosing PDF from a list

---
+++ ChoosePDFFromList/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace ChoosePDFFromList;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    /// <summary>
+    /// ListView ItemTapped event handler to update the PDF document stream
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void ListView_ItemTapped(object sender, ItemTappedEventArgs e)
+    {
+        if (this.BindingContext is PdfData bindingContext)
+        {
+            // Update the document stream
+            bindingContext.UpdateDocumentStream(e.Item.ToString());
+        }
+    }
+}
+
