6ae4b9e7a68ed7b8e73211c46a3f0d88a08c10e6
Deepak G <65169077+Deepak1799@users.noreply.github.com>
2023-08-14 15:04:13 +0530

    Sample to customize the busy indicator is added
    The sample demonstrates how to customize the color of the busy indicator (activity indicator) used in the PDF Viewer

---
+++ Styles/BusyIndicatorStyle/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace BusyIndicatorStyle;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Styles/BusyIndicatorStyle/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace BusyIndicatorStyle;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
