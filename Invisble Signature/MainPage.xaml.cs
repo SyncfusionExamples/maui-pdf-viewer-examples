25ad9bfa8a46dd6b20d96069555d63a16287f804
Deepak G <deepakgunasekaran@syncfusion.com>
2024-01-05 14:39:08 +0530

    Invisible signature example is added

---
+++ Invisble Signature/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace InvisbleSignatureDemo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
