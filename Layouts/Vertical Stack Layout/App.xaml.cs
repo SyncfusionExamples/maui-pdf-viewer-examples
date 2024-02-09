3cf6fa0f5815a27e50c0a1618a3715a1ddb5eb3e
Deepak G <deepakgunasekaran@syncfusion.com>
2024-02-09 17:13:24 +0530

    Folder structure changed

---
+++ Layouts/Vertical Stack Layout/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PdfViewerInStackLayout
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
