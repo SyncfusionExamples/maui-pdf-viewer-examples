3e8643a527d498424d79c3a887b280db94094c43
Salman Baris <salmanbaris.s@syncfusion.com>
2024-02-09 18:23:36 +0530

    Added the share pdf sample

---
+++ Sharing PDF document/SharePDF/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SharePDF
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
