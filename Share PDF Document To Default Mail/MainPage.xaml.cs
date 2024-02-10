dbc9a5e76991f3384efab9c05d2a8242ed85b3c6
Salman Baris <salmanbaris.s@syncfusion.com>
2024-02-10 23:44:52 +0530

    Added sample for default mail service support

---
+++ Share PDF Document To Default Mail/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿namespace SharePdfDocumentToDefaultMail
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            PdfViewer.DocumentSource = this.GetType().Assembly.GetManifestResourceStream("SharePdfDocumentToDefaultMail.Assets.PdfSuccinctly.pdf");
+        }
+
+        private async void SharePdfToDefaultMail_Clicked(object sender, EventArgs e)
+        {
+            string fileName = Path.Combine(FileSystem.Current.AppDataDirectory, "PDF_FromSfPdfViewer.pdf");
+
+            // Save the PDF document to the specified file
+            using (FileStream pdfStream = File.Create(fileName))
+            {
+                // Assuming pdfViewer is an instance of a class that can save the PDF document
+                PdfViewer.SaveDocument(pdfStream);
+            }
+
+            // Check if the file exists
+            if (File.Exists(fileName))
+            {
+                // Create a temporary file with the PDF content
+                var tempFilePath = Path.Combine(FileSystem.CacheDirectory, "PDF_FromSfPdfViewer.pdf");
+
+                // Copy the PDF content to the temporary file
+                File.Copy(fileName, tempFilePath, true);
+
+                // Share the PDF via email
+                if (Email.Default.IsComposeSupported)
+                {
+                    string subject = "PDF document from MAUI SfPdfViewer";
+                    string body = "Check out the Pdf document";
+
+                    var message = new EmailMessage
+                    {
+                        Subject = subject,
+                        Body = body,
+                        BodyFormat = EmailBodyFormat.PlainText,
+                        To = new List<string> { "recipient@example.com" }
+                    };
+
+                    // Attach the modified PDF file to the email
+                    message.Attachments?.Add(new EmailAttachment(tempFilePath));
+
+                    await Email.ComposeAsync(message);
+                }
+            }
+        }
+    }
+
+}
