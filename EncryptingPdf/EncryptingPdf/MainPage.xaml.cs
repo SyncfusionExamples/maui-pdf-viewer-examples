4990dca8fb7da8f06256dc09dfdb30ccf93bbaa5
Bharathi4332 <bharathi.selvam@syncfusion.com>
2024-02-16 14:05:46 +0530

    865890 - How to encrypt a PDF using PDF library and load them in PdfViewer?

---
+++ EncryptingPdf/EncryptingPdf/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿
+namespace EncryptingPdf
+{
+    public partial class MainPage : ContentPage
+    {
+       public MainPage()
+        {
+            InitializeComponent();
+        }
+     
+    }
+
+}
+
+
+
