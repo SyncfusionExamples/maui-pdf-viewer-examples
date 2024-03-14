eaf996947d71a7b5ffa29fcdca42189730721667
Bharathi4332 <bharathi.selvam@syncfusion.com>
2024-03-14 15:02:09 +0530

    committed Signature sample

---
+++ Annotations/ElectronicSignature/ElectronicSignature/Platforms/Android/AndroidDirectoryConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace ElectronicSignature
+{
+    static class AndroidDirectoryConstants
+    {
+        public const string PrimaryStorage = "primary";
+        public const string Storage = "storage";
+        public const string ExternalStorageBaseUrl = "content://com.android.externalstorage.documents/document/primary%3A";
+    }
+}
