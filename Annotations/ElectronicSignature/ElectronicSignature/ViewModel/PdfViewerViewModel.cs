2c6e4f245212595cb4debb3a66c12167e78785e2
Bharathi4332 <bharathi.selvam@syncfusion.com>
2024-03-14 16:43:58 +0530

    updated the signature sample

---
+++ Annotations/ElectronicSignature/ElectronicSignature/ViewModel/PdfViewerViewModel.cs
@@ -0,0 +1,56 @@
+﻿using Syncfusion.Maui.PdfViewer;
+using System.ComponentModel;
+using System.IO;
+using System.Windows.Input;
+
+namespace ElectronicSignature
+{
+    public class PdfViewerViewModel : INotifyPropertyChanged
+    {
+        private PdfFileData? fileData;
+        public Command OpenCommand { get; }
+        /// <summary>
+        /// Occurs when a property is changed.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        
+        async void OnOpenCommand()
+        {
+            var fileData = await FileService.OpenFile("pdf");
+            if (fileData != null)
+            {
+                FileData = fileData;
+            }
+        }
+        public PdfFileData? FileData
+        {
+            get { return fileData; }
+            set
+            {
+                if (fileData != value)
+                {
+                    fileData = value;
+                    OnPropertyChanged(nameof(FileData));
+                }
+            }
+        }
+
+        public PdfViewerViewModel()
+        {
+            Stream? PdfDocumentStream = this.GetType().Assembly.GetManifestResourceStream("ElectronicSignature.handwritten-signature.pdf");
+            FileData = new PdfFileData("handwritten-signature.pdf", PdfDocumentStream);
+            OpenCommand = new Command(OnOpenCommand);
+
+        }
+
+        /// <summary>
+        /// Trigges the property changed event.
+        /// </summary>
+        public void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+                       
+    }
+}
