a400230b004c77e4df218eff476315f26fa1e3a3
VijayadharshiniMathiyalagan <vijayadharshini.m@syncfusion.com>
2024-03-18 18:19:22 +0530

    comments added

--- ConversionOfCoordinates/ConversionOfCoordinates/MainPage.xaml.cs
+++ ConversionOfCoordinates/ConversionOfCoordinates/MainPage.xaml.cs
@@ -9,23 +9,33 @@ namespace ConversionOfCoordinates
 
         public MainPage()
         {
-            InitializeComponent();
+            // Load PDF document from embedded resource
             stream = typeof(MainPage).GetTypeInfo().Assembly.GetManifestResourceStream("ConversionOfCoordinates.Annotations.pdf");
             pdfViewer.LoadDocument(stream);
+
+            // Set background color of tool bar
             layout.BackgroundColor = Color.FromArgb("#F7F2FB");
         }
 
+        // Method to handle button click event
         private void Button_Clicked(object sender, EventArgs e)
         {
             int count = 0;
+
+            // Iterate through annotations in PDF viewer
             foreach (var annotation in pdfViewer.Annotations)
             {
+                // Check if annotation is a square annotation and it's the first one
                 if (annotation is SquareAnnotation squareAnnotation && count == 0)
                 {
                     count++;
-                    Point clientPoint = pdfViewer.ConvertPagePointToScrollPoint(new Point(squareAnnotation.Bounds.X, squareAnnotation.Bounds.Y), 2);
+
+                    // Convert annotation coordinates to scroll point coordinates
+                    Point scrollPoint = pdfViewer.ConvertPagePointToScrollPoint(new Point(squareAnnotation.Bounds.X, squareAnnotation.Bounds.Y), 2);
                     Point endPoint = pdfViewer.ConvertPagePointToScrollPoint(new Point(squareAnnotation.Bounds.X + squareAnnotation.Bounds.Width, squareAnnotation.Bounds.Y + squareAnnotation.Bounds.Height), 2);
-                    pdfViewer.ScrollToOffset(clientPoint.X, clientPoint.Y);
+
+                    // Scroll to annotation and select it
+                    pdfViewer.ScrollToOffset(scrollPoint.X, scrollPoint.Y);
                     pdfViewer.SelectAnnotation(squareAnnotation);
                 }
             }
