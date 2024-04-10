33f14899aa9b17ef068b69e7717007672bbb9353
Bharathi4332 <bharathi.selvam@syncfusion.com>
2024-04-10 19:06:22 +0530

    Committing the 865890 - How to encrypt a PDF using PDF library and load them in PdfViewer?

---
+++ EncryptingPDF/EncryptingPDF/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace EncryptingPDF
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }      
+    }
+
+}
---
+++ EncryptingPDF/EncryptingPDF/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace EncryptingPDF
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
