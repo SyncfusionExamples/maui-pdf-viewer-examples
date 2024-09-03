574f155c83063c051dfa2a8c280f3d76c6f4ab49
DeepakG <deepakguansekaran@syncfusion.com>
2024-09-03 23:28:50 +0530

    mail id content is updated

--- AI Demos/AISmartPDFFormFill/SmartFill/SmartFillViewModel.cs
+++ AI Demos/AISmartPDFFormFill/SmartFill/SmartFillViewModel.cs
@@ -36,9 +36,9 @@ namespace SmartFill
             string basePath = "SmartFill.PDF";
             // Load a PDF file as a resource stream
             _pdfFile = this.GetType().Assembly.GetManifestResourceStream($"{basePath}.smart-form.pdf");
-            UserDetail1 = "Hi, this is John. You can contact me at john123@gmail.com. I am male, born on February 20, 2005. I want to subscribe to a newspaper and learn courses, specifically a Machine Learning course. I am from Alaska.";
-            UserDetail2 = "S David here. You can reach me at David123@gmail.com. I am male, born on March 15, 2003. I would like to subscribe to a newspaper and am interested in taking a Digital Marketing course. I am from New York.";
-            UserDetail3 = "Hi, this is Alice. You can contact me at alice456@gmail.com. I am female, born on July 15, 1998. I want to unsubscribe from a newspaper and learn courses, specifically a Cloud Computing course. I am from Texas.";
+            UserDetail1 = "Hi, this is John. You can contact me at john123@emailid.com. I am male, born on February 20, 2005. I want to subscribe to a newspaper and learn courses, specifically a Machine Learning course. I am from Alaska.";
+            UserDetail2 = "S David here. You can reach me at David123@emailid.com. I am male, born on March 15, 2003. I would like to subscribe to a newspaper and am interested in taking a Digital Marketing course. I am from New York.";
+            UserDetail3 = "Hi, this is Alice. You can contact me at alice456@emailid.com. I am female, born on July 15, 1998. I want to unsubscribe from a newspaper and learn courses, specifically a Cloud Computing course. I am from Texas.";
         }
     }
 }
