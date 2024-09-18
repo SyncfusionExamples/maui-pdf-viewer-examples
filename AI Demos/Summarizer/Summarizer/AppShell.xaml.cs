6761bfb62ed382f4398c82235928420508eef0ed
SilambharasanSF4548 <166014892+KSilambharasanSF4548@users.noreply.github.com>
2024-09-18 17:29:07 +0530

    PDF Summarizer sample .net MAUI

---
+++ AI Demos/Summarizer/Summarizer/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Summarizer
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ AI Demos/Summarizer/Summarizer/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Summarizer
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
