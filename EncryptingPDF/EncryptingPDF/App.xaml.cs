33dae25bb78389744196c2e09af2ae741c998128
Bharathi4332 <bharathi.selvam@syncfusion.com>
2025-03-03 13:06:29 +0530

    Committing the changes for the KB task 865890

---
+++ EncryptingPDF/EncryptingPDF/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace EncryptingPDF
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Add license key");
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
