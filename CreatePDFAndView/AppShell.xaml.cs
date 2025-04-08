4590f9cf45b4c908c28c4980ff893ee399c3fed8
Bharathi4332 <bharathi.selvam@syncfusion.com>
2025-04-08 23:37:29 +0530

    Committing the sample with new name

---
+++ CreatePDFAndView/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CreatePDFAndView
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CreatePDFAndView/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CreatePDFAndView
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+      
+    }
+
+}
