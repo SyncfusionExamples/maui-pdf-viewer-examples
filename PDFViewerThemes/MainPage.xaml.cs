4d85ec8088f75923bb4c0d9e4ab7984da2d16767
Bharathi4332 <bharathi.selvam@syncfusion.com>
2025-05-19 17:06:04 +0530

    Committing the sample for applying custom themes using keys

---
+++ PDFViewerThemes/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace PDFViewerThemes
+{
+    public partial class MainPage : ContentPage
+    {
+      
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+       
+    }
+
+}
---
+++ PDFViewerThemes/PdfViewerViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel;
+using System.Reflection;
+
+namespace PDFViewerThemes
+{
+    class PdfViewerViewModel : INotifyPropertyChanged
+    {
+        private Stream? pdfDocumentStream;
+
+        /// <summary>
+        /// Occurs when a property value changes.
+        /// </summary>
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        /// <summary>
+        /// Gets or sets the stream of the currently loaded PDF document.
+        /// </summary>
+        public Stream? PdfDocumentStream
+        {
+            get
+            {
+                return pdfDocumentStream;
+            }
+            set
+            {
+                pdfDocumentStream = value;
+                OnPropertyChanged(nameof(PdfDocumentStream));
+            }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PdfViewerViewModel"/> class.
+        /// </summary>
+        public PdfViewerViewModel()
+        {
+            // Load the embedded PDF document stream.
+            pdfDocumentStream = typeof(App).GetTypeInfo().Assembly.GetManifestResourceStream("PDFViewerThemes.Assets.form_document.pdf");
+        }
+
+        /// <summary>
+        /// Raises the <see cref="PropertyChanged"/> event for the specified property name.
+        /// </summary>
+        /// <param name="name">The name of the property that changed.</param>
+        public void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
