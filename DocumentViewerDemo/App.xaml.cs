bd46b20f4e3acf51cc9d6b79572ae8294bc913f3
Nandhakumar-SF4686 <nandhakumar.sundarajan@syncfusion.com>
2025-08-27 13:43:14 +0530

    977905 - Create a blog sample for how to open various type of documents (.docx, xlsx, HTML) in MAUI PDF Viewer

---
+++ DocumentViewerDemo/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace DocumentViewerDemo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1JEaF1cWWhAYVJ0WmFZfVtgcV9GYlZRRmYuP1ZhSXxWdk1hXH9WcXBVTmNaVEJ9XEI=");
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
