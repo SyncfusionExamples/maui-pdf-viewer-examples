425a2b6cad49506168203b1be9d5c6b3c783074f
Nandhakumar-SF4686 <nandhakumar.sundarajan@syncfusion.com>
2025-10-10 15:32:46 +0530

    Adding changes

--- DetectPdfChanges/App.xaml.cs
+++ DetectPdfChanges/App.xaml.cs
@@ -4,6 +4,7 @@
     {
         public App()
         {
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Add Valid License");
             InitializeComponent();
         }
 
