36530dcd2f53dcc016708630ba348454ad08afc5
Bharathi Selvam <bharathi.selvam@syncfusion.com>
2026-03-31 00:22:29 +0530

    Committing the localization sample

---
+++ Localization/SfPdfViewerPage.xaml.cs
@@ -0,0 +1,45 @@
+using Syncfusion.Maui.PdfViewer;
+using System.Diagnostics;
+using System.Reflection;
+using System.Resources;
+
+namespace Localization;
+
+public partial class SfPdfViewerPage : ContentPage
+{
+    public SfPdfViewerPage(string selectedLanguage)
+    {
+        // Implement logic to set culture based on language
+        string cultureCode = selectedLanguage switch
+        {
+            "German" => "de-DE",
+            "Arabic" => "ar-AE",
+            _ => "en-US",
+        };
+
+        if (Application.Current is App app)
+        {
+            app.SetCulture(cultureCode);
+        }
+
+        SfPdfViewerResources.ResourceManager = new ResourceManager(
+            "Localization.Resources.SfPdfViewer",
+            typeof(App).Assembly
+        );
+
+        InitializeComponent();
+        LoadPdfWithLocalization();    
+    }
+    
+    protected async override void OnDisappearing()
+    {
+        base.OnDisappearing();
+        await pdfViewer.UnloadDocumentAsync();
+    }
+    private async void LoadPdfWithLocalization()
+    {        
+        // Load the PDF document
+        var documentStream = typeof(App).GetTypeInfo().Assembly.GetManifestResourceStream("Localization.Assets.PDF_Succinctly.pdf");
+        await pdfViewer.LoadDocumentAsync(documentStream);
+    }
+}
